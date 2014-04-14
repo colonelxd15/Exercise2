8a89d842ceb780844583edf1acaad6b8aff25746
Norman Manantan <normanmanantan@gmail.com>
2014-04-14 18:27:22 +0800

    Animation

--- JS/imageslider.js
+++ JS/imageslider.js
@@ -2,24 +2,109 @@ jQuery(document).ready(function ($) {
 
     var num = 1;
     var counter = 1;
-    var element = new Array();
-    var clickedElem = 0;
+    
+    $('#control_prev').click(left);
+    $('#control_next').click(right);
+  
+    $('#link1').on('click',function(){
+        openContent();
+        content(1);
+    });
+    $('#link2').on('click',function(){
+        openContent();
+        content(2);
+    });
+    $('#link3').on('click',function(){
+        openContent();
+        content(3);
+    });
+    $('#link4').on('click',function(){
+        openContent();
+        content(4);
+    });
+    $('#link5').on('click',function(){
+        openContent();
+        content(5);
+    });
+    
+    $('.close').click(function(){
+        contents();
+        closeContent();
+    });
+   
+    function right(){ 
+        $('#holder').animate(
+            {'margin-left': '-' + (num * 330) + 'px'} 
+            ,500
+            ,'easeOutExpo'
+            ,function(){ 
+                $('#holder .content:first-child').appendTo('#holder'); 
+                $('#holder').css('margin-left',''); 
+            }
+        ); 
+    };
+    
+    function left() {
+        $('#holder .content:last-child').prependTo('#holder'); 
+        $('#holder')
+            .css('margin-left', '-' + (num * 330) +'px')
+            .animate({ 'margin-left': ''},500,'easeOutExpo'); 
+    }; 
+    
+    function openContent(){ 
+        $('.content').animate(
+            {'width':'990px','display':'block'} 
+            ,300 
+            ,'easeOutExpo'
+            ,linkOpenAppear());
+    };
 
-    $('#control_prev').click(function(){ left(); });
-    $('#control_next').click(function(){ right(); });
+    function closeContent(){ 
+        $('.content').animate({'width':'330px'}
+            ,100
+            ,'easeOutExpo'
+            ,linkOpenDisappear()); 
+    };
 
-    $('#link1').click(openContent);
-    $('#link2').click(openContent);
-    $('#link3').click(openContent);
-    $('#link4').click(openContent);
-    $('#link5').click(openContent);
+    function linkOpenAppear(){ 
+        $('.data span').animate({'right':'-100px'}
+            ,200
+            ,'easeOutExpo');
+             num = 3; 
+    };
 
-    $('.close').click(closeContent);
-
-    function right(){ $('#holder').animate({'margin-left': '-' + (num * 330) + 'px'} ,500,'easeOutExpo' ,function(){ $('#holder .content:first-child').appendTo('#holder'); $('#holder').css('margin-left',''); }); };
-    function left() { $('#holder .content:last-child').prependTo('#holder'); $('#holder').css('margin-left', '-' + (num * 330) +'px'); $('#holder').animate({ 'margin-left': ''},500,'easeOutExpo'); };
-    function openContent(){ $('.content').animate({'width':'990px'} ,300 ,'easeOutExpo',linkOpenAppear()); };
-    function closeContent(){ $('.content').animate({'width':'330px'},100,'easeOutExpo',linkOpenDisappear()); };
-    function linkOpenAppear(){ $('.data span').animate({'right':'-100px'},200,'easeOutExpo'); num = 3; };
-    function linkOpenDisappear(){ $('.data span').animate({'right':'0px'},200,'easeOutExpo'); $('.data span').css({'right':'0px'}); num = 1; };
+    function linkOpenDisappear(){ 
+        $('.data span').animate({'right':'0px'} ,200,'easeOutExpo') 
+            .css({'right':'0px'}); 
+        num = 1; 
+    };    
+    
+    function content(i)
+    {
+        var index = 1;  
+    
+        while(index <= 5)
+        {
+            if(index != i)
+            {
+                $('#content' + index).css('display','none');
+            }
+            else
+            {
+               $('#content' + index).css('display','block');
+            }
+        while($('#content' + index) != $('#holder')
+            index++;
+        }
+    }
+    
+    function contents()
+    {
+        var index = 1;  
+        while(index <= 5)
+        {
+            $('#content' + index).css('display','block');
+            index++;
+        }
+    }
 });
