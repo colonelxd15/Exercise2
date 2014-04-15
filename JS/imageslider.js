29fb21d525b22dd4dd5e0bc0c330afef8d71cc21
Norman Manantan <normanmanantan@gmail.com>
2014-04-15 11:16:23 +0800

    Finished

--- JS/imageslider.js
+++ JS/imageslider.js
@@ -2,33 +2,45 @@ jQuery(document).ready(function ($) {
 
     var num = 1;
     var counter = 1;
-    
+    var position;
+
     $('#control_prev').click(left);
     $('#control_next').click(right);
   
     $('#link1').on('click',function(){
+        getChildPosition("content1");
         openContent();
         content(1);
+        contents();
     });
     $('#link2').on('click',function(){
+        getChildPosition("content2");
         openContent();
         content(2);
+        contents();
     });
     $('#link3').on('click',function(){
+        getChildPosition("content3");
         openContent();
         content(3);
+        contents();
     });
     $('#link4').on('click',function(){
+        getChildPosition("content4");
         openContent();
         content(4);
+        contents();
     });
     $('#link5').on('click',function(){
+        getChildPosition("content5");
         openContent();
         content(5);
+        contents();
     });
     
     $('.close').click(function(){
         contents();
+        back();
         closeContent();
     });
    
@@ -93,11 +105,35 @@ jQuery(document).ready(function ($) {
             {
                $('#content' + index).css('display','block');
             }
-        while($('#content' + index) != $('#holder')
             index++;
         }
     }
     
+    function getChildPosition(handler){
+        console.log(handler);
+        console.log("--------");
+        console.log($('#holder .content').get(0).id);
+        console.log($('#holder .content').get(1).id);
+        console.log($('#holder .content').get(2).id);
+
+        if(handler === ($('#holder .content').get(0).id))
+        {
+            position = "first";       
+        }
+        else if(handler === ($('#holder > .content').get(1).id)){
+
+            $('#holder .content:first-child').appendTo('#holder'); 
+            position = "second";
+        }
+        else if(handler === ($('#holder > .content').get(2).id)){
+            $('#holder .content:first-child').appendTo('#holder'); 
+            $('#holder .content:first-child').appendTo('#holder'); 
+            position = "third";
+        }        
+
+    }
+
+
     function contents()
     {
         var index = 1;  
@@ -105,6 +141,21 @@ jQuery(document).ready(function ($) {
         {
             $('#content' + index).css('display','block');
             index++;
+        }
+    }
+    function back(){
+        if(position == "first")
+        {
+
+        }
+        else if(position == "second")
+        {
+            $('#holder .content:last-child').prependTo('#holder');
+        }
+        else if(position == "third")
+        {
+            $('#holder .content:last-child').prependTo('#holder'); 
+            $('#holder .content:last-child').prependTo('#holder');
         }
     }
 });
